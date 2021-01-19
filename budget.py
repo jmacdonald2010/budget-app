d70d7c727ab15c0e2e337d47e101642dd282ab25
jmacdonald2010 <jamesdmacdonaldiii@gmail.com>
2021-01-19 09:45:58 -0500

    in progress 1-19-21 0945

---
+++ budget.py
@@ -0,0 +1,33 @@
+class Category:
+    # some stuff here
+
+    x = 0
+
+    def __init__(self, category_name):
+        self.category_name = category_name
+    
+    def deposit(self, amount, description=""):
+        # function goes here
+        pass
+
+    def withdraw(self, amount, description=""):
+        # function go here
+        pass
+
+    def get_balance(self):
+        # no inputs, just gets the current get_balance
+        pass
+
+    def transfer(self, amount, category):
+        # function go here
+        pass
+
+    def check_funds(self, amount):
+        pass
+
+
+
+def create_spend_chart(categories):
+    # some other stuff
+    pass
+
---
+++ changelog.md
@@ -0,0 +1,2 @@
+1/19/2021 09:45
+Created the basic outline for the class. Have not written any functions yet. Still planning.
