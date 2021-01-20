101ceb392e6b7525ed37dce10f4309b653e7c17c
jmacdonald2010 <jamesdmacdonaldiii@gmail.com>
2021-01-19 22:25:40 -0500

    in progress 1-19-2021 2225 commit

--- budget.py
+++ budget.py
@@ -1,33 +1,65 @@
 class Category:
     # some stuff here
 
-    x = 0
+    # x = 0 # not sure if this is needed
+    ledger = []
 
     def __init__(self, category_name):
         self.category_name = category_name
     
     def deposit(self, amount, description=""):
         # function goes here
-        pass
+        # this might be all this needs? not yet tested 1/19/21 2200
+        self.ledger.append({"amount": amount, "description": description})
 
     def withdraw(self, amount, description=""):
         # function go here
-        pass
+        # step one, calculate if the amt input into this method would cause the ledger amount to go negative
+        # might be it? not yet tested 1/19/21 2200
+        balance = self.get_balance()
+        if balance < 0:
+            return False
+        else:
+            amount = amount * -1
+            self.ledger.append({"amount": amount, "description": description})
+            return True
 
     def get_balance(self):
         # no inputs, just gets the current get_balance
-        pass
+        balance = 0 
+        for i in self.ledger:
+            balance = balance + i["amount"]
+        return balance
 
     def transfer(self, amount, category):
         # function go here
-        pass
+        # NOTE THIS NEEDS TO USE THE check_funds METHOD
+        self.withdraw(amount, f"Transfer to {category}") # i think this may satisfy the use check_funds method for this method
+        category.deposit(amount, f"Transfer from {self.category_name}") # i have no idea if this works like this or not, just kinda guessing
 
     def check_funds(self, amount):
-        pass
+        balance = self.get_balance()
+        if amount > balance:
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        # use this to return the formatted ledger?
+        pass # temporary until i write this function
 
 
 
 def create_spend_chart(categories):
     # some other stuff
-    pass
+    # calc how many *'s need to be used w/ the category name (top line only 30 characters)
+    # this code actually shouldn't go here
+    # this should be in the budget class, and is printed when the budget object is printed 
+    # plus more than this
+    category_length = len(categories)
+    stars_needed = 30 - category_length
+    title_line = ("*" * int((stars_needed / 2)))
+    title_line = title_line + categories
+    title_line = title_line + ("*" * int((stars_needed / 2)))
+    return title_line.rstrip()
 
--- changelog.md
+++ changelog.md
@@ -1,2 +1,5 @@
+1/19/2021 22:24
+Wrote some of the class methods. Have not tested them yet, so they will likely all need work. Hopefully, the comments in the code reflect the changes that have been made so far and those that still need to be made.
+
 1/19/2021 09:45
 Created the basic outline for the class. Have not written any functions yet. Still planning.
